76e6f9b81739497337096c96105dc595f30ec7ad
Daniel Chang <daniel.chang85@gmail.com>
2014-11-30 14:18:13 -0500

    PENDING writing block_available method

--- app/services/schedule_block_recommender.rb
+++ app/services/schedule_block_recommender.rb
@@ -3,7 +3,8 @@ class ScheduleBlockRecommender
 
   DEFAULT_START = "tomorrow at 7:00"
   DEFAULT_END_TIME = 22 # hour at 10:00pm
-  DEFAULT_INTERVAL = 1800 # 30 minutes
+  DEFAULT_INTERVAL = 30.minutes
+  NEXT_DAY = 9.hours
 
   def self.get_recommended_dates(user, interview=nil)
     new(user, interview).get_recommended_dates
@@ -27,23 +28,49 @@ class ScheduleBlockRecommender
   end
 
   def create_possible_recommendations_for_interview
-    recommendations_count = interview.possible_interview_blocks.count
+    recommendations_count = current_possible_interviews
+    block_start_time = Chronic.parse(DEFAULT_START)
 
-    until recommendations_count == 25
+    until recommendations_count == 15
+
+      if block_available?(block_start_time)
+        interview.possible_interview_blocks.create(
+          start_time: block_start_time, end_time: time_plus_30_min(block_start_time)
+          )
+        recommendations_count += 1
+      end
+
+      increment_time_by_30_min_or_to_next_day(block_start_time)
     end
   end
 
-# 2) break up next day into 30 min blocks. Chronic.parse("tomorrow at 7:00")...
-# 3) CYCLE -- every 30 min block |block|
-    # a) block exists in range of any interview_rejected_blocks?
-      # particularly, this means:
 
-        # block.starts_between?(interv_rej_block) || block.ends_between?(interv_rej_block)
+  private
+    def block_available?(block_start_time)
+      # interview.interview_rejected_blocks.where()
 
-        # def block.starts_between?(interv_rej_block)
-        #   block.start_time < interv_rej_block.end_time && block.start_time > interv_rej_block.start_time
-        # end
+       # a) block exists in range of any interview_rejected_blocks?
+          # particularly, this means:
 
-    # b) block exists in range of any user_rejected_blocks? similar to above
-    # continue until you have 25 of these. recommend first 3.
+            # block.starts_between?(interv_rej_block) || block.ends_between?(interv_rej_block)
+
+            # def block.starts_between?(interv_rej_block)
+            #   block.start_time < interv_rej_block.end_time && block.start_time > interv_rej_block.start_time
+            # end
+
+        # b) block exists in range of any user_rejected_blocks? similar to above
+        # continue until you have 25 of these. recommend first 3.
+    end
+
+    def time_plus_30_min(time)
+      time + DEFAULT_INTERVAL
+    end
+
+    def increment_time_by_30_min_or_to_next_day(block_start_time)
+      block_start_time += (block_start_time == DEFAULT_END_TIME ? NEXT_DAY : DEFAULT_INTERVAL)
+    end
+
+    def current_possible_interviews
+      interview.possible_interview_blocks.count
+    end
 end
