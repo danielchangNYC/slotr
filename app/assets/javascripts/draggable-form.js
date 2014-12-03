176e9cb477c65b4799c6fc9a0a0d47f4512d05f2
Daniel Chang <daniel.chang85@gmail.com>
2014-12-03 00:25:20 -0500

    Draggable schedule UI

---
+++ app/assets/javascripts/draggable-form.js
@@ -0,0 +1,18 @@
+$(function(){
+  var list = document.querySelector('ul.schedule-list');
+
+  list.addEventListener('slip:reorder', function(e){
+    e.target.parentNode.insertBefore(e.target, e.detail.insertBefore);
+    return false;
+  }, false);
+
+  list.addEventListener('slip:beforewait', function(e){
+    if (e.target.className.indexOf('instant') > -1) e.preventDefault();
+  }, false);
+
+    list.addEventListener('slip:afterswipe', function(e){
+      $(e.target).html('');
+      // Add data-possible-date-id to form
+    }, false);
+  new Slip(list);
+});
